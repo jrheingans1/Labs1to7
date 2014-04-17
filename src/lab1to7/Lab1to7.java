b84cd09c0e80ad94ae0cf47bc28f49d4e8dd8855
jrheingans1 <jrheingans1@L11709038603.wctc.msft>
2014-04-17 17:26:51 -0500

    Lab 1 done?

---
+++ src/lab1to7/Lab1to7.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1to7;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author jrheingans1
+ */
+public class Lab1to7 {
+
+    private String lastName;
+    private String firstName;
+    private String ssn;
+
+    public Lab1to7(String lastName, String firstName, String ssn) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.ssn = ssn;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.ssn);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Lab1to7 other = (Lab1to7) obj;
+        if (!Objects.equals(this.ssn, other.ssn)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Lab1to7{" + "lastName=" + lastName + ", firstName=" + firstName + ", ssn=" + ssn + '}';
+    }
+
+}
