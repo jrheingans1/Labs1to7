defdc6fbd711b9981bd05abf26f0fa37833aca7a
jrheingans1 <jrheingans1@L11709038603.wctc.msft>
2014-04-17 17:50:27 -0500

    Working on Lab 2

---
+++ src/lab1to7/Lab2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab1to7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jrheingans1
+ */
+public class Lab2 {
+    Lab1 employee1 = new Lab1("James", "Rheingans", "333-33-3333");
+    Lab1 employee2 = new Lab1("John", "Smith", "444-44-4444");
+    Lab1 employee3 = new Lab1("Sally", "Jones", "222-22-2222");
+    Lab1 employee4 = new Lab1("James", "Rheingans", "333-33-3333");
+    
+    (String)List employeeList = new ArrayList();
+    
+    
+    public static void main(String[] args) {
+        System.out.println(listSize());
+    }
+    
+    
+}
